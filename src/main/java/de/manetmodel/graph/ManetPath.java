1086f82c9b02dc9db6618ffd71b95d80fda49349
klement <klement.streit@gmail.com>
2020-11-23 17:39:33 +0100

    Fixed return error

--- src/main/java/de/manetmodel/graph/ManetPath.java
+++ src/main/java/de/manetmodel/graph/ManetPath.java
@@ -6,63 +6,77 @@ import java.util.Set;
 
 import de.manetmodel.util.Tuple;
 
-public class ManetPath<N extends ManetVertex, L extends ManetEdge> extends LinkedList<Tuple<L,N>> implements Comparable<ManetPath<N, L>>{
+public class ManetPath<N extends ManetVertex, L extends ManetEdge> extends LinkedList<Tuple<L, N>>
+		implements Comparable<ManetPath<N, L>>
+{
 
 	double distance;
 	Set<Integer> occupation;
-	
-	public ManetPath(N source) {
-		this.add(new Tuple<L,N>(null, source));
+
+	public ManetPath(N source)
+	{
+		this.add(new Tuple<L, N>(null, source));
 		this.occupation = new HashSet<Integer>();
-		this.distance = 0;	
+		this.distance = 0;
 	}
-	
-	public N getSource() {
-		this.get(0).getSecond();
+
+	public N getSource()
+	{
+		return this.get(0).getSecond();
 	}
-	
-	public boolean isShorter(ManetPath<N, L> path) {		
-		if(path != null) return this.distance < path.getDistance();				
+
+	public boolean isShorter(ManetPath<N, L> path)
+	{
+		if (path != null)
+			return this.distance < path.getDistance();
 		return false;
 	}
-	
-	public double getDistance() {
+
+	public double getDistance()
+	{
 		return this.distance;
 	}
-	
-	public Set<Integer> getOccupation(){
+
+	public Set<Integer> getOccupation()
+	{
 		return this.occupation;
 	}
-	
+
 	@Override
-	public boolean add(Tuple<L,N> linkAndNode) {
-		super.add(linkAndNode);		
+	public boolean add(Tuple<L, N> linkAndNode)
+	{
+		super.add(linkAndNode);
 		this.distance += linkAndNode.getFirst().getDistance();
 		this.occupation.addAll(linkAndNode.getFirst().getOccupation());
 		return true;
-	}		
-		
-	@Override
-	public String toString() {		
-		String str = "";				
-		for(Tuple<L,N> linkAndNode: this) {	
-			if(linkAndNode.getFirst() != null)
-				str += String.format("- %.2f -", linkAndNode.getFirst().getDistance());	
-			str += String.format("[%s]", linkAndNode.getSecond().toString());	
-		}		
-		return str;	
 	}
-	
-	public int compareDistanceTo(ManetPath<N, L> path) {
+
+	@Override
+	public String toString()
+	{
+		String str = "";
+		for (Tuple<L, N> linkAndNode : this)
+		{
+			if (linkAndNode.getFirst() != null)
+				str += String.format("- %.2f -", linkAndNode.getFirst().getDistance());
+			str += String.format("[%s]", linkAndNode.getSecond().toString());
+		}
+		return str;
+	}
+
+	public int compareDistanceTo(ManetPath<N, L> path)
+	{
 		return Double.compare(this.distance, path.getDistance());
 	}
-	
-	public int compareOccupationTo(ManetPath<N, L> path) {
+
+	public int compareOccupationTo(ManetPath<N, L> path)
+	{
 		return Integer.compare(this.occupation.size(), path.getOccupation().size());
 	}
 
 	@Override
-	public int compareTo(ManetPath<N, L> path) {
-		return Double.compare(this.distance, path.getDistance());				
-	}	
+	public int compareTo(ManetPath<N, L> path)
+	{
+		return Double.compare(this.distance, path.getDistance());
+	}
 }
