08eb67274bba4c8c05fcc0295b9d52fc77659215
klement <klement.streit@gmail.com>
2020-11-26 16:39:56 +0100

    bitrate

--- src/main/java/de/manetmodel/network/Flow.java
+++ src/main/java/de/manetmodel/network/Flow.java
@@ -1,12 +1,21 @@
 package de.manetmodel.network;
 
 import de.manetmodel.graph.ManetPath;
-import de.manetmodel.util.Tuple;
 
-public class Flow<N extends Node, L extends Link> extends ManetPath<N, L>{
+public class Flow<N extends Node, L extends Link> extends ManetPath<N, L>
+{
 
-	public Flow(N source) {
-		super(source);
+	private double bitrate;
+
+	public Flow(N source, N target)
+	{
+		this(source, target, 0d);
+	}
+
+	public Flow(N source, N target, double bitrate)
+	{
+		super(source, target);
+		this.bitrate = bitrate;
 	}
 
 }
