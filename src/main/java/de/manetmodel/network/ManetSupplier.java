dcb4bf2aaf81caae1dd42ba3a6badd9d76ce7526
klement <klement.streit@gmail.com>
2020-11-28 13:29:15 +0100

    Manet Link and Node Supplier

---
+++ src/main/java/de/manetmodel/network/ManetSupplier.java
@@ -0,0 +1,32 @@
+package de.manetmodel.network;
+
+import java.util.function.Supplier;
+
+public class ManetSupplier implements Supplier<Manet<Node, Link>>
+{
+	@Override
+	public Manet<Node, Link> get()
+	{
+		Manet<Node, Link> graph = new Manet<Node, Link>(new ManeNodeSupplier(), new ManetLinkSupplier());
+		return graph;
+	}
+
+	public static class ManetLinkSupplier implements Supplier<Link>
+	{
+		@Override
+		public Link get()
+		{
+			return new Link();
+		}
+	}
+
+	public static class ManeNodeSupplier implements Supplier<Node>
+	{
+		@Override
+		public Node get()
+		{
+			return new Node();
+		}
+	}
+
+}
