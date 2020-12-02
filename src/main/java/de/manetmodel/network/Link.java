dc113e4c6e682c4403814d9f2ccd2911c074d274
klement <klement.streit@gmail.com>
2020-12-02 13:34:13 +0100

    getter for interferedNodes

--- src/main/java/de/manetmodel/network/Link.java
+++ src/main/java/de/manetmodel/network/Link.java
@@ -1,22 +1,33 @@
 package de.manetmodel.network;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import de.manetmodel.graph.IManetElement;
 import de.manetmodel.graph.ManetEdge;
 
-public class Link<N, Q> extends ManetEdge implements IManetElement
+public class Link<N extends Node, Q> extends ManetEdge implements IManetElement
 {
 	private Q linkQuality;
 	/*
 	 * Links that are occupied due to transmission
 	 */
-	private Set<N> interferredLinks;
+	private Set<N> interferredNodes;
+
+	public Link()
+	{
+		interferredNodes = new HashSet<N>();
+	}
 
 	public void setInterferredNodes(List<N> list)
 	{
-		interferredLinks.addAll(list);
+		interferredNodes.addAll(list);
+	}
+
+	public Set<N> getInterferenceNodes()
+	{
+		return this.interferredNodes;
 	}
 
 	public Q getLinkQuality()
