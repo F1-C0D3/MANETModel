a2d9f1993c198111e88167401dbc2f71d372d103
eikeviehmann <eike.viehmann@unibw.de>
2020-12-13 17:01:37 +0100

    Update/Fix RandomPath

--- src/main/java/de/manetmodel/algo/RandomPath.java
+++ src/main/java/de/manetmodel/algo/RandomPath.java
@@ -1,8 +1,6 @@
 package de.manetmodel.algo;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import de.manetmodel.graph.Edge;
 import de.manetmodel.graph.Path;
@@ -12,13 +10,14 @@ import de.manetmodel.util.RandomNumbers;
 import de.manetmodel.util.Tuple;
 
 public class RandomPath<V extends Vertex, E extends Edge> {
-    
+
     private WeightedUndirectedGraph<V, E> graph;
 
     public RandomPath(WeightedUndirectedGraph<V, E> graph) {
 	this.graph = graph;
     }
-
+    
+    /*
     public Path<V, E> compute(V source, V target) {
 	Path<V, E> randomPath = new Path<V, E>();
 	Set<E> visited = new HashSet<E>();
@@ -39,52 +38,40 @@ public class RandomPath<V extends Vertex, E extends Edge> {
 	}
 	return randomPath;
     }
+    
+    */
+      
+    public Path<V, E> compute(V source, V target) {
 
-    public Path<V, E> compute(V source, double distance) {
+	Path<V, E> randomPath = new Path<V, E>(source, target);
 
-	Path<V, E> randomPath = new Path<V, E>();
-	Set<E> visited = new HashSet<E>();
+	while (!randomPath.isComplete()) {
 
-	while (randomPath.getDistance() < distance) {
+	    List<V> nextHops = randomPath.getUnvisitedVertices(graph.getNextHopsOf(randomPath.getCurrentVertex()));
 
-	    List<E> edges = graph.getEdgesOf(source);
-
-	    edges.removeIf(e -> visited.contains(e));
-
-	    if (!edges.isEmpty()) {
-		E edge = edges.get(RandomNumbers.getRandom(0, edges.size()));
-		visited.add(edge);
-		randomPath.add(new Tuple<E, V>(edge, source));
-		source = graph.getTargetOf(source, edge);
-	    } else {
-		randomPath.clear();
-		return randomPath;
+	    if (!nextHops.isEmpty()) {
+		V nextHop = nextHops.get(RandomNumbers.getRandom(0, nextHops.size()));
+		randomPath.add(new Tuple<E, V>(graph.getEdge(randomPath.getCurrentVertex(), nextHop), nextHop));
 	    }
-	}
+	    else return new Path<V, E>(source, target);
+	}	
 	return randomPath;
     }
-    
+
     public Path<V, E> compute(V source, int hops) {
 
-	Path<V, E> randomPath = new Path<V, E>();
-	Set<E> visited = new HashSet<E>();
+	Path<V, E> randomPath = new Path<V, E>(source);
 
-    	for(int i=0; i<hops; i++) {
+	for (int i = 0; i < hops; i++) {
 
-	    List<E> edges = graph.getEdgesOf(source);
+	    List<V> nextHops = randomPath.getUnvisitedVertices(graph.getNextHopsOf(randomPath.getCurrentVertex()));
 
-	    edges.removeIf(e -> visited.contains(e));
-
-	    if (!edges.isEmpty()) {
-		E edge = edges.get(RandomNumbers.getRandom(0, edges.size()));
-		visited.add(edge);
-		randomPath.add(new Tuple<E, V>(edge, source));
-		source = graph.getTargetOf(source, edge);
-	    } else {
-		randomPath.clear();
-		return randomPath;
+	    if (!nextHops.isEmpty()) {
+		V nextHop = nextHops.get(RandomNumbers.getRandom(0, nextHops.size()));
+		randomPath.add(new Tuple<E, V>(graph.getEdge(randomPath.getCurrentVertex(), nextHop), nextHop));
 	    }
 	}
+	
 	return randomPath;
     }
 }
