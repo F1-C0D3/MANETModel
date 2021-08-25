43e7d6989605fd5ebfcc316a106b759ba90f7ce2
eikeviehmann <eike.viehmann@unibw.de>
2021-08-25 09:41:24 +0200

    Adjust OverUtilizationProblemGeneratorTest

--- stc/test/java/de/manetmodel/generator/OverUtilzedProblemGeneratorTest.java
+++ stc/test/java/de/manetmodel/generator/OverUtilzedProblemGeneratorTest.java
@@ -16,20 +16,15 @@ import de.jgraphlib.graph.generator.GraphProperties.IntRange;
 import de.jgraphlib.gui.VisualGraphApp;
 import de.jgraphlib.util.RandomNumbers;
 import de.manetmodel.gui.LinkQualityPrinter;
-import de.manetmodel.network.Link;
-import de.manetmodel.network.LinkQuality;
-import de.manetmodel.network.MANETSupplier;
-import de.manetmodel.network.Node;
+
 import de.manetmodel.network.example._Flow;
+import de.manetmodel.network.example._Link;
+import de.manetmodel.network.example._LinkQuality;
 import de.manetmodel.network.example._MANET;
 import de.manetmodel.network.example._MANETSupplier;
-import de.manetmodel.network.mobility.PedestrianMobilityModel;
+import de.manetmodel.network.example._Node;
 import de.manetmodel.network.radio.ScalarRadioModel;
 import de.manetmodel.network.unit.DataRate;
-import de.manetmodel.network.unit.Speed;
-import de.manetmodel.network.unit.Time;
-import de.manetmodel.network.unit.Unit;
-import de.manetmodel.network.unit.Speed.SpeedRange;
 
 public class OverUtilzedProblemGeneratorTest {
   
@@ -38,28 +33,25 @@ public class OverUtilzedProblemGeneratorTest {
 
 	_MANET manet = new _MANET(new _MANETSupplier().getNodeSupplier(), new _MANETSupplier().getLinkSupplier(),
 		new _MANETSupplier().getLinkQualitySupplier(), new _MANETSupplier().getMyFlowSupplier(),
-		new ScalarRadioModel(0.002d, 1e-11, 1000d, 2412000000d),
-		new PedestrianMobilityModel(RandomNumbers.getInstance(10),
-			new SpeedRange(4d, 40d, Unit.Time.hour, Unit.Distance.kilometer),
-			new Time(Unit.Time.second, 30l), new Speed(4d, Unit.Distance.kilometer, Unit.Time.hour), 10));
+		new ScalarRadioModel(0.002d, 1e-11, 1000d, 2412000000d));
 
 	NetworkGraphProperties graphProperties = new NetworkGraphProperties(/* playground width */ 1024,
 		/* playground height */ 768, /* number of vertices */ new IntRange(100, 100),
 		/* distance between vertices */ new DoubleRange(50d, 100d),
 		/* edge distance */ new DoubleRange(100d, 100d));
 
-	NetworkGraphGenerator<Node, Link<LinkQuality>, LinkQuality> generator = new NetworkGraphGenerator<Node, Link<LinkQuality>, LinkQuality>(
-		manet, new MANETSupplier().getLinkPropertySupplier(), new RandomNumbers());
+	NetworkGraphGenerator<_Node, _Link, _LinkQuality> generator = new NetworkGraphGenerator<_Node, _Link, _LinkQuality>(
+		manet, new _MANETSupplier().getLinkQualitySupplier(), new RandomNumbers());
 	
 	generator.generate(graphProperties);
 	manet.initialize();
 
-	Function<LinkQuality, Double> metric = (LinkQuality w) -> {
+	Function<_LinkQuality, Double> metric = (_LinkQuality w) -> {
 	    return (double) w.getUtilization().get();
 	};
 
-	OverUtilzedProblemGenerator<Node, Link<LinkQuality>, LinkQuality, _Flow> overUtilizedProblemGenerator 
-		= new OverUtilzedProblemGenerator<Node, Link<LinkQuality>, LinkQuality, _Flow>(
+	OverUtilzedProblemGenerator<_Node, _Link, _LinkQuality, _Flow> overUtilizedProblemGenerator 
+		= new OverUtilzedProblemGenerator<_Node, _Link, _LinkQuality, _Flow>(
 			manet, metric);
 
 	OverUtilizedProblemProperties problemProperties = new OverUtilizedProblemProperties();
@@ -72,7 +64,7 @@ public class OverUtilzedProblemGeneratorTest {
 
 	List<_Flow> flowProblems = overUtilizedProblemGenerator.compute(problemProperties);
 	
-	SwingUtilities.invokeAndWait(new VisualGraphApp<Node, Link<LinkQuality>, LinkQuality>(manet, flowProblems, new LinkQualityPrinter()));
+	SwingUtilities.invokeAndWait(new VisualGraphApp<_Node, _Link, _LinkQuality>(manet, new LinkQualityPrinter<_LinkQuality>()));
 	
 	System.in.read();
     }
