b9fc7456a7d5c7568628808393cd1b1f30952539
KlementStreit <klement.streit@gmail.com>
2021-08-31 18:17:13 +0200

    Adapted LinkQualityPrinter since necessary edgeWeights are not computed yet

--- src/main/java/de/manetmodel/gui/LinkQualityPrinter.java
+++ src/main/java/de/manetmodel/gui/LinkQualityPrinter.java
@@ -7,8 +7,13 @@ public class LinkQualityPrinter<W extends LinkQuality> extends EdgeWeightPrinter
    
     @Override
     public String print(W edgeWeight) {
-	double percentage = ((double) edgeWeight.getUtilization().get() / (double) edgeWeight.getTransmissionRate().get()) * 100d;	
-	return String.format("%.2f%%", percentage);
+//	double percentage = ((double) edgeWeight.getUtilization().get() / (double) edgeWeight.getTransmissionRate().get()) * 100d;	
+//	return String.format("%.2f%%", percentage);
+	
+	//Paramteres not yet obtained. 
+	return edgeWeight.getDistance().toString();
+	
+	
     }
     
 }	
