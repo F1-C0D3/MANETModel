05353eda9dce212e3dc03b247fa407f9c46d9684
KlementStreit <klement.streit@gmail.com>
2021-09-14 18:04:36 +0200

    RadioModel provides maxTransmissionRange information

--- src/main/java/de/manetmodel/radiomodel/RadioModel.java
+++ src/main/java/de/manetmodel/radiomodel/RadioModel.java
@@ -7,15 +7,15 @@ import de.manetmodel.units.DataRate;
 
 public abstract class RadioModel<N extends Node,L extends Link<W>, W extends LinkQuality> {
 
-    private DataRate transmissionBitrate;
-    private final double linkMaxTransmissionRange;
+    protected DataRate transmissionBitrate;
+    protected final double linkMaxTransmissionRange;
 
     public RadioModel(DataRate transmissionBitrate, double linkMaxTransmissionRange) {
 	this.linkMaxTransmissionRange = linkMaxTransmissionRange;
 	this.transmissionBitrate = transmissionBitrate;
     }
     
-    public double getLinkMaxTransmissionRate() {
+    public double getLinkMaxTransmissionRange() {
 	return linkMaxTransmissionRange;
     }
 
