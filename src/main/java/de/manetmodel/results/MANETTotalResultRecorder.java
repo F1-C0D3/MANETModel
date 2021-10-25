ad981185ca42c988e2f80a4f3fae66925dfa0ed0
Klement Streit <klement.streit@unibw.de>
2021-10-25 13:39:39 +0200

    Changed finished method -> more generic

--- src/main/java/de/manetmodel/results/MANETTotalResultRecorder.java
+++ src/main/java/de/manetmodel/results/MANETTotalResultRecorder.java
@@ -9,22 +9,27 @@ import de.manetmodel.network.LinkQuality;
 import de.manetmodel.network.Node;
 import de.manetmodel.results.CSVExporter.RecordType;
 
-public class MANETTotalResultRecorder<T extends ResultParameter, I extends IndividualRunResultParameter,A extends AverageRunResultParameter>
-	extends TotalResultRecorder<T,I,A> {
+public class MANETTotalResultRecorder<T extends ResultParameter, I extends IndividualRunResultParameter, A extends AverageRunResultParameter>
+	extends TotalResultRecorder<T, I, A> {
 
-    private TotalResultMapper<T,I,A> mapper;
-    public MANETTotalResultRecorder(String resultFileName, TotalResultMapper<T,I,A> mapper) {
-	super(resultFileName);
-	this.mapper=mapper;
+    private TotalResultMapper<T, I, A> mapper;
+
+    public MANETTotalResultRecorder(String resultFileName, TotalResultMapper<T, I, A> mapper,
+	    List<RunResultContent<I, A>> runResultContents) {
+	super(resultFileName, runResultContents);
+	this.mapper = mapper;
 
     }
 
     @Override
-    public void finish(List<RunResultContent<I, A>> runResultContents) {
+    public void finish() {
+	if (super.resultContents.size() > 0) {
+	    CSVExporter exporter = new CSVExporter(resultFileName, RecordType.total);
+	    T result = mapper.toalMapper(super.resultContents);
+	    exporter.write(Arrays.asList(result), mapper.getTotalMappingStrategy(), mapper.getScenario(),
+		    RecordType.total.toString());
+	}
 
-	CSVExporter exporter = new CSVExporter(resultFileName, RecordType.total);
-	T result = mapper.toalMapper(runResultContents);
-	exporter.write(Arrays.asList(result), mapper.getTotalMappingStrategy(), mapper.getScenario(), "total");
-//	
     }
+
 }
